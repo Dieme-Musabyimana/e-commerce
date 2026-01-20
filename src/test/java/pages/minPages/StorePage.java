85142c38b0a0d89491a3491cad7014af23b8dc34
Dieme-Musabyimana <musabyimanadieme10@gmail.com>
2026-01-20 12:58:10 +0200

    removing unneccessary comments

--- src/test/java/pages/minPages/StorePage.java
+++ src/test/java/pages/minPages/StorePage.java
@@ -24,6 +24,65 @@ public class StorePage extends BasePage {
         super(driver);
         PageFactory.initElements(driver, this);
     }
-
+    public void load(String url) {
+        driver.get(url);
+    }
+    public void addProductToCart(String productName){
+        String selector = "a[aria-label*='" + productName + "']";
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(selector))).click();
     }
 
+    public  void viewCartFromStorePage() {
+        WebElement viewCart = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[title='View cart']")));
+        viewCart.click();
+
+    }
+    public void gotToProductPage(String productName){
+
+        String urlSlug = productName.toLowerCase().replace(" ", "-");
+        String selector = String.format("a[href*='product/%s']", urlSlug);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(selector))).click();
+    }
+
+
+    public String selectCategory(String categoryName) {
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("product_cat"))).click();
+        String selector = "//option[contains(text(), '" + categoryName + "')]";
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(selector))).click();
+        return driver.getTitle();
+
+    }
+    public ProductPage goToProductPage(String productPageName){
+        String selector = "//h2[normalize-space()='" + productPageName + "']";
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(selector))).click();
+        return new ProductPage(driver);
+    }
+
+//    public void filterByPrice(double minPrice, double maxPrice) {
+//        List<WebElement> oldProducts = driver.findElements(By.cssSelector(".product"));
+//
+//        WebElement leftHandle = driver.findElement(By.cssSelector(".ui-slider-handle:nth-of-type(1)"));
+//        WebElement rightHandle = driver.findElement(By.cssSelector(".ui-slider-handle:nth-of-type(2)"));
+//        WebElement sliderBar = driver.findElement(By.cssSelector(".price_slider"));
+//
+//        int totalWidth = sliderBar.getSize().getWidth();
+//
+//        Actions action = new Actions(driver);
+//        action.dragAndDropBy(leftHandle, -totalWidth, 0).build().perform(); // Reset to 0
+//
+//
+//        int leftOffset = (int) ((minPrice / 150) * totalWidth);
+//        action.dragAndDropBy(leftHandle, leftOffset, 0).build().perform();
+//
+//        action.dragAndDropBy(rightHandle, totalWidth, 0).build().perform(); // Reset to far right
+//        int rightOffset = (int) (((maxPrice - 150) / 150) * totalWidth);
+//        action.dragAndDropBy(rightHandle, rightOffset, 0).build().perform();
+//
+//        wait.until(ExpectedConditions.elementToBeClickable(filterButton)).click();
+//
+//        if (!oldProducts.isEmpty()) {
+//            wait.until(ExpectedConditions.stalenessOf(oldProducts.get(0)));
+//        }
+//    }
+}
