4e7aca811af41abbcf7bbc16c0abf22b4e65fc33
Dieme-Musabyimana <musabyimanadieme10@gmail.com>
2026-03-05 23:09:07 +0200

    messsage

--- src/test/java/runner/runnerClass.java
+++ src/test/java/runner/runnerClass.java
@@ -1,3 +1,34 @@
+//package runner;
+//
+//import io.cucumber.testng.AbstractTestNGCucumberTests;
+//import io.cucumber.testng.CucumberOptions;
+//import org.testng.annotations.DataProvider;
+//
+//@CucumberOptions(
+//        features = {"src/test/resources/features/cartFeatures/addToCart_Guest.feature"},
+//        glue = {"stepDefinitions.cartSteps", "hooks"},
+//        plugin = {
+//                "pretty",
+//                "json:target/cucumber.json",
+//                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+//                // >>>>>>>>>>>>>>>>>>>>>> EXTENT REPORT <<<<<<<<<<<<<<<<<<<<
+//                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+//                // >>>>>>>>>>>>>>>>>>>>>> EXTENT REPORT <<<<<<<<<<<<<<<<<<<<
+//
+//                // >>>>>>>>>>>>>>>>>>>>>> ALLURE REPORT <<<<<<<<<<<<<<<<<<<<
+//                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+//                // >>>>>>>>>>>>>>>>>>>>>> ALLURE REPORT <<<<<<<<<<<<<<<<<<<<
+//        },
+//        monochrome = true
+//)
+//public class runnerClass extends AbstractTestNGCucumberTests {
+//
+//    @Override
+//    @DataProvider(parallel = false)
+//    public Object[][] scenarios() {
+//        return super.scenarios();
+//    }
+//}
 package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
@@ -10,14 +41,9 @@ import org.testng.annotations.DataProvider;
         plugin = {
                 "pretty",
                 "json:target/cucumber.json",
+                "junit:target/surefire-reports/cucumber.xml", // CRITICAL FOR AZURE TESTS TAB
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-                // >>>>>>>>>>>>>>>>>>>>>> EXTENT REPORT <<<<<<<<<<<<<<<<<<<<
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-                // >>>>>>>>>>>>>>>>>>>>>> EXTENT REPORT <<<<<<<<<<<<<<<<<<<<
-
-                // >>>>>>>>>>>>>>>>>>>>>> ALLURE REPORT <<<<<<<<<<<<<<<<<<<<
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
-                // >>>>>>>>>>>>>>>>>>>>>> ALLURE REPORT <<<<<<<<<<<<<<<<<<<<
         },
         monochrome = true
 )
