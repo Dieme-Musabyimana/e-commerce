6b4eff9ca9821940f126c3a2131c1f44a80c8133
Dieme-Musabyimana <musabyimanadieme10@gmail.com>
2026-03-05 13:57:13 +0200

    Complete BDD framework updates for Checkout and Cart functionality

--- src/test/java/pages/minPages/StorePage.java
+++ src/test/java/pages/minPages/StorePage.java
@@ -1,16 +1,13 @@
 package pages.minPages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import pages.base.BasePage;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class StorePage extends BasePage {
--- src/test/java/runner/runnerClass.java
+++ src/test/java/runner/runnerClass.java
@@ -5,9 +5,20 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
-        features = {"src/test/resources/features/"},
-        glue = {"stepDefinitions", "hooks"},
-        plugin = {"pretty", "html:target/cucumber-reports.html"},
+        features = {"src/test/resources/features/cartFeatures/addToCart_Guest.feature"},
+        glue = {"stepDefinitions.cartSteps", "hooks"},
+        plugin = {
+                "pretty",
+                "json:target/cucumber.json",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                // >>>>>>>>>>>>>>>>>>>>>> EXTENT REPORT <<<<<<<<<<<<<<<<<<<<
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                // >>>>>>>>>>>>>>>>>>>>>> EXTENT REPORT <<<<<<<<<<<<<<<<<<<<
+
+                // >>>>>>>>>>>>>>>>>>>>>> ALLURE REPORT <<<<<<<<<<<<<<<<<<<<
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+                // >>>>>>>>>>>>>>>>>>>>>> ALLURE REPORT <<<<<<<<<<<<<<<<<<<<
+        },
         monochrome = true
 )
 public class runnerClass extends AbstractTestNGCucumberTests {
